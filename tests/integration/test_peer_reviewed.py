3fcd20c6e8d2ab7561ec9b3919d608bed0fdc772
Mattia Passeri <mattia.passeri2@studio.unibo.it>
2022-05-24 18:42:26 +0200

    Made tests faster and refactored tests package structure

--- tests/integration/test_peer_reviewed.py
+++ tests/integration/test_peer_reviewed.py
@@ -4,14 +4,15 @@ import re
 import unittest
 from multiprocessing import Process
 
-from examples.federated.client import setup_client
-from examples.federated.server import setup_server
+from examples.peer_reviewed.client import setup_client
+from examples.peer_reviewed.server import setup_server
 
-LOGGING_FILE = "./tests/test_federated.log"
+LOGGING_FILE = "./tests/test_peer_reviewed.log"
+TRAIN_TEST_FRACTION = 0.1
 NUM_CLASSES = 10
 NUM_CLIENTS = 2
 FL_ROUNDS = 1
-PORT = 8089
+PORT = 8090
 
 
 def run_fl():
@@ -20,7 +21,7 @@ def run_fl():
 
     clients = list()
     for i in range(NUM_CLIENTS):
-        c = Process(target=setup_client, args=(PORT, NUM_CLIENTS, i))
+        c = Process(target=setup_client, args=(PORT, NUM_CLIENTS, i, TRAIN_TEST_FRACTION))
         c.start()
         clients.append(c)
 
@@ -29,7 +30,7 @@ def run_fl():
     server.join()
 
 
-class TestFederatedTraining(unittest.TestCase):
+class TestPeerReviewedTraining(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.setup_done = False
@@ -45,18 +46,30 @@ class TestFederatedTraining(unittest.TestCase):
             return
         if os.path.exists(LOGGING_FILE):
             os.remove(LOGGING_FILE)
-        run_fl()
+
+        server = Process(target=setup_server, args=(PORT, FL_ROUNDS, LOGGING_FILE))
+        server.start()
+
+        clients = list()
+        for i in range(NUM_CLIENTS):
+            c = Process(target=setup_client, args=(PORT, NUM_CLIENTS, i))
+            c.start()
+            clients.append(c)
+
+        for c in clients:
+            c.join()
+        server.join()
         self.setup_done = True
 
     def test_logging_file(self):
         self.assertTrue(os.path.exists(LOGGING_FILE))
 
-    def test_fl_finished(self):
+    def test_pr_finished(self):
         with open(LOGGING_FILE, "r") as f:
             lines = f.readlines()
             self.assertGreater(sum([1 for line in lines if "FL finished" in line]), 0)
 
-    def test_fl_loss(self):
+    def test_pr_loss(self):
         with open(LOGGING_FILE, "r") as f:
             lines = f.readlines()
             for line in lines:
